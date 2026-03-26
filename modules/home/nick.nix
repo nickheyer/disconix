fac60ad3ec8ccdd6fa5c526bd95fd2741ca1804e
nickheyer <nick@heyer.app>
2026-03-25 21:52:41 -0700

    kitty

--- modules/home/nick.nix
+++ modules/home/nick.nix
@@ -90,9 +90,80 @@
         programs.kitty = {
           enable = true;
           settings = {
-            font_size = 11;
-            background_opacity = "0.9";
+            # FONT
             font_family = "MesloLGS Nerd Font";
+            font_size = 11;
+            disable_ligatures = "never";
+
+            # WINDOW
+            background_opacity = "0.85";
+            window_padding_width = 12;
+            placement_strategy = "center";
+            hide_window_decorations = "yes";
+            confirm_os_window_close = 0;
+
+            # CURSOR
+            cursor_shape = "beam";
+            cursor_beam_thickness = "1.5";
+            cursor_blink_interval = 0;
+
+            # SCROLLBACK
+            scrollback_lines = 10000;
+
+            # TABS
+            tab_bar_edge = "bottom";
+            tab_bar_style = "powerline";
+            tab_powerline_style = "slanted";
+
+            # URL
+            url_style = "curly";
+            detect_urls = "yes";
+
+            # BELL
+            enable_audio_bell = "no";
+            visual_bell_duration = "0.0";
+
+            # CATPPUCCIN MOCHA
+            foreground = "#CDD6F4";
+            background = "#1E1E2E";
+            selection_foreground = "#1E1E2E";
+            selection_background = "#F5E0DC";
+            cursor = "#F5E0DC";
+            cursor_text_color = "#1E1E2E";
+            url_color = "#F5E0DC";
+            active_border_color = "#B4BEFE";
+            inactive_border_color = "#6C7086";
+            bell_border_color = "#F9E2AF";
+            active_tab_foreground = "#11111B";
+            active_tab_background = "#CBA6F7";
+            inactive_tab_foreground = "#CDD6F4";
+            inactive_tab_background = "#181825";
+            tab_bar_background = "#11111B";
+
+            # BLACK
+            color0 = "#45475A";
+            color8 = "#585B70";
+            # RED
+            color1 = "#F38BA8";
+            color9 = "#F38BA8";
+            # GREEN
+            color2 = "#A6E3A1";
+            color10 = "#A6E3A1";
+            # YELLOW
+            color3 = "#F9E2AF";
+            color11 = "#F9E2AF";
+            # BLUE
+            color4 = "#89B4FA";
+            color12 = "#89B4FA";
+            # MAGENTA
+            color5 = "#F5C2E7";
+            color13 = "#F5C2E7";
+            # CYAN
+            color6 = "#94E2D5";
+            color14 = "#94E2D5";
+            # WHITE
+            color7 = "#BAC2DE";
+            color15 = "#A6ADC8";
           };
         };
 
