49e2cf22bab8738603f89b50b6a3252442aad976
nickheyer <nick@heyer.app>
2026-03-26 18:29:35 -0700

    cursor theme

--- modules/home/nick.nix
+++ modules/home/nick.nix
@@ -35,6 +35,14 @@
           libnotify
         ];
 
+        # NIRI
+        home.pointerCursor = {
+          name = "catppuccin-mocha-dark-cursors";
+          package = pkgs.catppuccin-cursors.mochaDark;
+          size = 24;
+          gtk.enable = true;
+        };
+
         # ZSH
         programs.zsh = {
           enable = true;
