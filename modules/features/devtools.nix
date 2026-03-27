8f5036dfa2d26628a2a3ce2b0209b510dd3dbb9c
nickheyer <nick@heyer.app>
2026-03-26 21:45:10 -0700

    upd

--- modules/features/devtools.nix
+++ modules/features/devtools.nix
@@ -1,7 +1,7 @@
 { ... }:
 {
   flake.nixosModules.devtools =
-    { pkgs, ... }:
+    { pkgs, lib, ... }:
     {
       # DOCKER
       virtualisation.docker.enable = true;
@@ -38,6 +38,18 @@
         pkg-config
         gtk3
         webkitgtk_4_1
+      ];
+
+      # PKGCFG
+      environment.variables.PKG_CONFIG_PATH = lib.makeSearchPath "lib/pkgconfig" [
+        pkgs.gtk3.dev
+        pkgs.webkitgtk_4_1.dev
+        pkgs.glib.dev
+        pkgs.cairo.dev
+        pkgs.pango.dev
+        pkgs.gdk-pixbuf.dev
+        pkgs.atk.dev
+        pkgs.harfbuzz.dev
       ];
     };
 }
