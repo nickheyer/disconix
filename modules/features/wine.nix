aaa732f89c96b485e9a44d88dbd5edc8de64418c
nickheyer <nick@heyer.app>
2026-04-13 23:03:52 -0700

    wine fixes

--- modules/features/wine.nix
+++ modules/features/wine.nix
@@ -12,10 +12,47 @@
       # 32-BIT WINE
       hardware.graphics.enable32Bit = true;
 
-      # LIBGL
+      # COMMON LIBS FOR UNPATCHED BINARIES (NIX-LD)
       programs.nix-ld.libraries = with pkgs; [
+        # GRAPHICS
         libGL
         libglvnd
+        vulkan-loader
+
+        # X11
+        libx11
+        libxext
+        libxcursor
+        libxrandr
+        libxi
+        libxfixes
+        libxinerama
+        libxscrnsaver
+        libxxf86vm
+
+        # WAYLAND
+        wayland
+        libxkbcommon
+
+        # AUDIO
+        alsa-lib
+        libpulseaudio
+        openal
+
+        # SDL
+        SDL2
+        SDL2_image
+        SDL2_ttf
+        SDL2_mixer
+
+        # COMMON RUNTIME DEPS
+        zlib
+        libffi
+        stdenv.cc.cc.lib
+        freetype
+        fontconfig
+        glib
+        dbus
       ];
     };
 }
